696b499595e8b9f23063632d01acb396eb9b49fa
you@example.com <you@example.com>
2019-02-27 09:46:12 +0700

    sec

--- js/main.js
+++ js/main.js
@@ -15,7 +15,26 @@ $(document).ready(function(){
             }
         });
         $('#mhs-list').html(result);
-        $('#mhs-select').html(gender_result);
+        $('#mhs-select').html("<option value='semua'>semua</option>"+gender_result);
 
 });
+
+$("#mhs-select").on('change',function(){
+    updateListMahasiswa($(this).val);
+});
+
+    function updateListMahasiswa(opt){
+        var result='';
+        var _url2=_url;
+        if(opt!=='semua'){
+            _url2=url +'?gender='+opt;   
+        }
+        $.get(_url,function(data){
+            $.each(data,function(key,items ){
+                _gend=items.gender;
+                result +='<div><h3>'+items.name+'</h3><p>'+_gend+'</p></div>';
+            });
+            $('#mhs-list').html(result);
+    });
+    }
 });
